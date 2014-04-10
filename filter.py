d3c4defcd07fc35c8e535ddb4351a05bb825511a
Suresh Alse <alseambusher@gmail.com>
2014-04-10 13:11:20 +0530

    filter added

---
+++ filter.py
@@ -0,0 +1,43 @@
+import os
+os.chdir("../res")
+all = open("photos.txt","r")
+soccer = open("soccer_events.txt","r")
+tech = open("technical_events.txt", "r")
+indi =open("indignados_events.txt","r")
+all_array = all.readlines()[0].split(",")
+soccer_array = soccer.readlines()[0].split(",")
+tech_array= tech.readlines()[0].split(",")
+indi_array =indi.readlines()[0].split(",")
+
+def get_array(array,num):
+	result = [sub if sub in array else None for sub in all_array]
+	result = [ x for x in result if x is not None ]
+	return ",".join(result[:num])
+
+soccer100 = get_array(soccer_array,100)
+tech100 = get_array(tech_array,100)
+indi100 = get_array(indi_array,100)
+
+soccer1000 = get_array(soccer_array,1000)
+tech1000 = get_array(tech_array,1000)
+indi1000 = get_array(indi_array,1000)
+
+#soccer100 = ",".join([filter(lambda x: x in soccer_array, sublist) for sublist in all_array][:100])
+#tech100 = ",".join([filter(lambda x: x in tech_array, sublist) for sublist in all_array][:100])
+#indi100 = ",".join([filter(lambda x: x in indi_array, sublist) for sublist in all_array][:100])
+
+#soccer1000 = ",".join([filter(lambda x: x in soccer_array, sublist) for sublist in all_array][:1000])
+#tech1000 = ",".join([filter(lambda x: x in tech_array, sublist) for sublist in all_array][:1000])
+#indi1000 = ",".join([filter(lambda x: x in indi_array, sublist) for sublist in all_array][:1000])
+
+soccer100_file=open("soccer100.txt","w")
+tech100_file=open("tech100.txt","w")
+indi100_file=open("indi100.txt","w")
+
+soccer1000_file=open("soccer1000.txt","w")
+tech1000_file=open("tech1000.txt","w")
+indi1000_file=open("indi1000.txt","w")
+
+soccer100_file.write(soccer100)
+tech100_file.write(tech100)
+indi100_file.write(indi100)
--- json_gen.py
+++ json_gen.py
@@ -4,6 +4,7 @@ import json
 #import signal
 
 data = {}
+images = []
 #def save_json(signal,frame):
 	#json.dump(data,file("features.json","w"))	
 	#exit(0)
@@ -15,6 +16,8 @@ for photo in photos:
 	#title = photo.getElementsByTagName("title")[0].firstChild.data
 	#title = title.split(" ")[0] # get only the first part
 	title = photo.getAttributeNode("id").nodeValue
+	if title not in images:
+		continue
 	time = photo.getAttributeNode("dateTaken").nodeValue
 	try:
 		location = photo.getElementsByTagName("location")[0]
@@ -22,9 +25,10 @@ for photo in photos:
 		longitude = location.getAttributeNode("longitude").nodeValue
 
 	except:
-		continue
+		lattitude = -1
+		longitude = -1
 	try:
-		tags = [ tag for tag in photo.getElementsByTagName("tag")]
+		tags = [ tag.firstChild.data in for tag in photo.getElementsByTagName("tag") ]
 	except:
 		continue
 	data[title] = [time,lattitude,longitude,tags]
