707a3532de673ad8ea67537c5ba549b4a5935fa7
Suresh Alse <alseambusher@gmail.com>
2014-04-14 13:03:25 +0530

    classification complete

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ import os
 import config
 import datetime
 from scipy.spatial.distance import cosine
+from geopy.distance import great_circle
+from sklearn.svm import SVC
 
 os.chdir(config.res)
 soccer100= open("soccer100.txt","r").readlines()[0].split(",")
@@ -30,7 +32,12 @@ def compute_distance(data,features1,features2):
 			time2 = datetime.datetime.strptime(features2[id2][0], "%Y-%m-%d %H:%M:%S.%f")
 			time = abs((time1-time2).total_seconds())
 
-			location = None
+			location1 = (features1[id1][1] ,features1[id1][2])
+			location2 = (features2[id2][1] ,features2[id2][2])
+			if location1 is (-1,-1) or location2 is (-1,-1):
+				location = -1
+			else:
+				location = great_circle(location1, location2).miles
 	
 			tag_sim = 0
 			for tag1 in features1[id1][3]:
@@ -45,8 +52,9 @@ def compute_distance(data,features1,features2):
 
 			mins["time"] = min(mins["time"],time)
 			maxs["time"] = max(mins["time"],time)
-			mins["location"] = min(mins["location"],location)
-			maxs["location"] = max(mins["location"],location)
+			if location is not -1:
+				mins["location"] = min(mins["location"],location)
+				maxs["location"] = max(mins["location"],location)
 			mins["tag_sim"] = min(mins["tag_sim"],tag_sim)
 			maxs["tag_sim"] = max(mins["tag_sim"],tag_sim)
 			mins["sift"] = min(mins["sift"],sift)
@@ -57,11 +65,25 @@ def compute_distance(data,features1,features2):
 	#normalize
 	for index in range(0,len(data)):
 		data[index][1][0] = normalize(mins["time"],max["time"],data[index][1][0])
-		data[index][1][1] = normalize(mins["location"],max["location"],data[index][1][1])
+		if data[index][1][1] is not -1:
+			data[index][1][1] = normalize(mins["location"],max["location"],data[index][1][1])/2
+		else:
+			data[index][1][1] = 1
 		data[index][1][2] = normalize(mins["tag_sim"],max["tag_sim"],data[index][1][2])
 		data[index][1][3] = normalize(mins["sift"],max["sift"],data[index][1][3])
+		data[index][1] = np.array(data[index][1])
 
 data_signal=[]
 data_test=[]
 compute_distance(data_signal,features["signal"],features["signal"])
 compute_distance(data_test,features["test"],features["signal"])
+print "distance complete"
+
+# svm
+clf = SVC()
+clf.fit([data[1] for data in data_signal] , [data[2] for data in data_signal])
+predicted=[]
+for data in data_test:
+	predicted.append([data[1],clf.predict(data[1]))
+
+json.dump({"data":predicted}, open("predicted.json","w"))
